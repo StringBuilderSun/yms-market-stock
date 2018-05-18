72d96181bd270e85b16736da0f3bbc493ec9ff46
StringBuilderSun <957143335@qq.com>
2018-05-18 20:35:02 +0800

    add file

---
+++ com-market-facade/src/main/java/com/market/stock/StockService.java
@@ -0,0 +1,8 @@
+package com.market.stock;
+
+/**
+ * Created by lijinpeng on 2018/5/18.
+ */
+public interface StockService {
+   void service();
+}
---
+++ com-market-service/src/main/java/com/market/stock/StockServiceImpl.java
@@ -0,0 +1,10 @@
+package com.market.stock;
+
+/**
+ * Created by lijinpeng on 2018/5/18.
+ */
+public class StockServiceImpl implements StockService {
+    public void service() {
+
+    }
+}
